f427268668e95816b2e8540607e9797bc484bda3
Piarre <pierre@ideestore.fr>
2023-12-04 21:45:22 +0100

    📝 Added doc for File.ts

--- src/Utils/file.ts
+++ src/Utils/file.ts
@@ -3,6 +3,13 @@ import { outro } from "@clack/prompts";
 import * as fs from "fs";
 import path from "path";
 
+/**
+ * Creates a new directory.
+ *
+ * @param {string} path - The path where the new directory should be created.
+ * @returns {boolean} Returns `true` if the directory was successfully created, and `false` if the directory already exists.
+ * @throws Will throw an error if the directory cannot be created due to insufficient permissions or other I/O error.
+ */
 export const mkdir = (path: string): boolean => {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
@@ -12,24 +19,39 @@ export const mkdir = (path: string): boolean => {
   }
 };
 
-export const rmdir = async (pathToDelete: string) => {
+/**
+ * Recursively removes a directory and its contents.
+ *
+ * @async
+ * @param {string} pathToDelete - The path of the directory to remove.
+ * @throws Will throw an error if the directory does not exist or an I/O error occurs.
+ */
+export const rmdir = async (pathToDelete: string): Promise<void> => {
   if (fs.existsSync(pathToDelete)) {
     const files = fs.readdirSync(pathToDelete);
 
-    files.forEach((fichier) => {
-      const cheminFichier = path.join(pathToDelete, fichier);
+    files.forEach((file) => {
+      const filePath = path.join(pathToDelete, file);
 
-      if (fs.statSync(cheminFichier).isDirectory()) {
-        rmdir(cheminFichier).catch((error) => error);
+      if (fs.statSync(filePath).isDirectory()) {
+        rmdir(filePath).catch((error) => error);
       } else {
-        fs.unlinkSync(cheminFichier);
+        fs.unlinkSync(filePath);
       }
     });
 
     fs.rmdirSync(pathToDelete);
   }
-};
+}
 
+/**
+ * Checks if a directory is empty.
+ *
+ * @async
+ * @param {string} path - The path of the directory to check.
+ * @returns {Promise<boolean>} A promise that resolves to `true` if the directory is empty, and `false` otherwise.
+ * @throws Will throw an error if the directory does not exist or an I/O error occurs.
+ */
 export const isEmpty = async (path: string): Promise<boolean> => {
   try {
     const files = await fsPromises.readdir(path);
