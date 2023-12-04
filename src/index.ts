04c0e27ba7e9885665ed424dd69ac4df7129db2e
Piarre <pierre@ideestore.fr>
2023-12-04 13:13:19 +0100

    ✨ Add NextJS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Command } from "commander";
 import { init } from "./Utils/Template/Typescript.js";
 import { TypeScriptProject } from "./types.js";
+import { next } from "./Utils/Template/Next.js";
 
 export const IDE = new Command();
 
@@ -16,6 +17,12 @@ IDE.command("ts")
   .option("-D, --devDependencies [devDependencies...]", "Additional dev dependencies")
   .description("Initialize a new TypeScript project (typescript, @types/node, tsup)")
   .action(async (option: TypeScriptProject) => await init(option));
+
+IDE.command("next")
+  .option("-n, --name <name>", "Project name")
+  .option("-g, --git", "Initialize git repository")
+  .description("Initialize a new Next.js project (typescript, next, react, react-dom)")
+  .action(async (option: TypeScriptProject) => await next(option));
 
 (async () => {
   IDE.parse(process.argv);
