b5270fc1d1fc601ddffcb8968c95159d24dce5d4
Piarre <pierre@ideestore.fr>
2023-12-04 13:13:07 +0100

    ✨ Exec from execa with custom cwd

--- src/Utils/exec.ts
+++ src/Utils/exec.ts
@@ -1,7 +1,6 @@
 import { execSync } from "child_process";
+import { execa } from "execa";
 
-export const exec = (cmd: string) => {
-  return execSync(cmd, {
-    stdio: "inherit",
-  });
-};
+const exec = async (command: string, args: any[] | string[], path?: string) => await execa(command, args, { cwd: path ?? process.cwd() });
+
+export default exec;
