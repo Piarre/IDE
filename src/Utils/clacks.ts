51e063cc7b698e95318f6273a4ac92aaccca26d5
Piarre <pierre@ideestore.fr>
2023-12-04 21:50:14 +0100

    🔥 Useless function

--- src/Utils/clacks.ts
+++
@@ -1,9 +0,0 @@
-import { isCancel, outro } from "@clack/prompts";
-import chalk from "chalk";
-
-export function Cancel(event: unknown, message?: string) {
-  if (isCancel(event)) {
-    outro(`🔥 ${chalk.red(message || "Exited")}`);
-    process.exit(0);
-  }
-}
